40a80f4d21dc8415005a8bf23196020f0f86fea0
Leandro Costa Magalhães Silva <leandrodevbackend@gmail.com>
2024-04-08 18:35:41 -0400

    update

--- BiciAutenteAndroid/app/src/main/java/com/example/biciautenteandroid/LoginActivity.kt
+++ BiciAutenteAndroid/app/src/main/java/com/example/biciautenteandroid/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.example.biciautenteandroid
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.biciautenteandroid.api.JavaApiService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
@@ -31,6 +36,9 @@ class LoginActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
                 // Aqui você pode adicionar o código para redirecionar o usuário para a próxima tela ou executar outras ações após o login bem-sucedido
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finaliza a atividade atual para impedir que o usuário retorne à tela de login pressionando o botão Voltar
             } else {
                 Toast.makeText(
                     this,
@@ -42,8 +50,30 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun validarLogin(cpf: String, senha: String): Boolean {
-        // Aqui você pode adicionar a lógica para validar o login com o banco de dados ou outro método de autenticação
-        // Por enquanto, vamos considerar que o login é válido se o CPF e a senha não estiverem vazios
-        return cpf.isNotBlank() && senha.isNotBlank()
+        // Verifica se o CPF e a senha não estão vazios
+        if (cpf.isBlank() || senha.isBlank()) {
+            return false
+        }
+
+        // Aqui você pode fazer a chamada à API para verificar se o login é válido
+        CoroutineScope(Dispatchers.Main).launch {
+            val response = JavaApiService.fazerLogin(cpf, senha)
+
+            // Se o login for bem-sucedido, inicia a MainActivity
+            if (response.isSuccessful) {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finaliza a atividade atual para impedir que o usuário retorne à tela de login pressionando o botão Voltar
+            } else {
+                // O login é inválido se a resposta da API indicar falha
+                Toast.makeText(
+                    this@LoginActivity,
+                    "CPF ou senha incorretos. Tente novamente.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        return true // Retorne true imediatamente, a validação será concluída assincronamente
     }
 }
