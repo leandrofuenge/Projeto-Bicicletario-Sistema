a1f36065398b1680b832d171eb20d745ff8c0152
Leandro Costa Magalhães Silva <leandrodevbackend@gmail.com>
2024-04-09 22:51:56 -0400

    update

--- BiciAutenteAndroid/app/MainScreen.js
+++ BiciAutenteAndroid/app/MainScreen.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
-const MainScreen = () => {
+const MainScreen = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Tela Principal</Text>
